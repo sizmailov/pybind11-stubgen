f56a061ca23f11ee348d8a636ed9216dc3157ef1
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-03-12 16:44:39 +0300

    turn into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='pybind11_stubgen',
+    maintainer="Sergei Izmailov",
+    maintainer_email="sergei.a.izmailov@gmail.com",
+    description="PEP 561 type stubs generator for pybind11 modules",
+    url="https://github.com/sizmailov/pybind11_stubgen",
+    version="0.0.0",
+    packages=['pybind11_stubgen']
+)
