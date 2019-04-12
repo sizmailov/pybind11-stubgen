065e1382a59bc902f5cce97826a6da240ff99d6f
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-04-12 14:38:48 +0300

    bump version

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     maintainer_email="sergei.a.izmailov@gmail.com",
     description="PEP 561 type stubs generator for pybind11 modules",
     url="https://github.com/sizmailov/pybind11_stubgen",
-    version="0.0.0",
+    version="0.0.1",
     packages=['pybind11_stubgen']
 )
