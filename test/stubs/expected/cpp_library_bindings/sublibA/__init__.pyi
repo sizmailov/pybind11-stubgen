e526a9f092acd5d619368ccba004ebd12ae8440d
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-07-31 17:50:33 +0300

    Update expected pyi

--- test/stubs/expected/cpp_library_bindings/sublibA/__init__.pyi
+++ test/stubs/expected/cpp_library_bindings/sublibA/__init__.pyi
@@ -5,7 +5,65 @@ from typing import Iterator as iterator
 from numpy import float64
 _Shape = Tuple[int, ...]
 __all__  = [
-"add"
+"ConsoleBackgroundColor",
+"ConsoleForegroundColor",
+"add",
+"Blue",
+"Green",
+"Magenta",
+"Yellow"
 ]
+class ConsoleBackgroundColor():
+    """Members:
+
+  Green
+
+  Yellow
+
+  Blue
+
+  Magenta"""
+    def __init__(self, arg0: int) -> None: ...
+    def __int__(self) -> int: ...
+    @property
+    def name(self) -> str:
+        """(self: handle) -> str
+
+:type: str"""
+    Blue = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Blue
+    Green = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Green
+    Magenta = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Magenta
+    Yellow = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Yellow
+    __entries = None # type: dict # value = {'Green': (ConsoleBackgroundColor.Green, None), 'Yellow': (ConsoleBackgroundColor.Yellow, None), 'Blue': (ConsoleBackgroundColor.Blue, None), 'Magenta': (ConsoleBackgroundColor.Magenta, None)}
+    __members__ = None # type: dict # value = {'Green': ConsoleBackgroundColor.Green, 'Yellow': ConsoleBackgroundColor.Yellow, 'Blue': ConsoleBackgroundColor.Blue, 'Magenta': ConsoleBackgroundColor.Magenta}
+    pass
+class ConsoleForegroundColor():
+    """Members:
+
+  Green
+
+  Yellow
+
+  Blue
+
+  Magenta"""
+    def __init__(self, arg0: int) -> None: ...
+    def __int__(self) -> int: ...
+    @property
+    def name(self) -> str:
+        """(self: handle) -> str
+
+:type: str"""
+    Blue = None # type: cpp_library_bindings.sublibA.ConsoleForegroundColor # value = ConsoleForegroundColor.Blue
+    Green = None # type: cpp_library_bindings.sublibA.ConsoleForegroundColor # value = ConsoleForegroundColor.Green
+    Magenta = None # type: cpp_library_bindings.sublibA.ConsoleForegroundColor # value = ConsoleForegroundColor.Magenta
+    Yellow = None # type: cpp_library_bindings.sublibA.ConsoleForegroundColor # value = ConsoleForegroundColor.Yellow
+    __entries = None # type: dict # value = {'Green': (ConsoleForegroundColor.Green, None), 'Yellow': (ConsoleForegroundColor.Yellow, None), 'Blue': (ConsoleForegroundColor.Blue, None), 'Magenta': (ConsoleForegroundColor.Magenta, None)}
+    __members__ = None # type: dict # value = {'Green': ConsoleForegroundColor.Green, 'Yellow': ConsoleForegroundColor.Yellow, 'Blue': ConsoleForegroundColor.Blue, 'Magenta': ConsoleForegroundColor.Magenta}
+    pass
 def add(arg0: int, arg1: int) -> int:
     pass
+Blue = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Blue
+Green = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Green
+Magenta = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Magenta
+Yellow = None # type: cpp_library_bindings.sublibA.ConsoleBackgroundColor # value = ConsoleBackgroundColor.Yellow
