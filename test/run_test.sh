9b0a223f8455bea43b9750ed7619779298e48f3b
Tobias Pfeiffer <tgp@preferred.jp>
2020-07-28 11:35:39 +0900

    use a fixed commit of pybind11-project-example for tests

--- test/run_test.sh
+++ test/run_test.sh
@@ -6,9 +6,11 @@ if [ ! -d pybind11-project-example ]
 then
     git clone https://github.com/sizmailov/pybind11-project-example.git
 else
-    (cd pybind11-project-example; git pull origin)
+    (cd pybind11-project-example; git fetch origin)
 fi
 
+# update the commit below when the example code changes and you want to extend the tests
+(cd pybind11-project-example; git checkout 2310344c932c404d8bcfa2c355f07dcfedcc4082)
 (cd pybind11-project-example; git submodule update --init --recursive)
 (cd pybind11-project-example; python setup.py install)
 
