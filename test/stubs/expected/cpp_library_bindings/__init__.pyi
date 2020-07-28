05a095faa789e915774b9295a0076840a8e4d484
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-07-28 13:35:04 +0300

    Test nested enum names

--- test/stubs/expected/cpp_library_bindings/__init__.pyi
+++ test/stubs/expected/cpp_library_bindings/__init__.pyi
@@ -7,6 +7,7 @@ _Shape = Tuple[int, ...]
 __all__  = [
 "CppException",
 "Foo",
+"Outer",
 "sublibA"
 ]
 class CppException(Exception, BaseException):
@@ -25,4 +26,46 @@ class Foo():
         pass
     def __init__(self) -> None: ...
     def f(self) -> None: ...
+    pass
+class Outer():
+    class Inner():
+        class NestedEnum():
+            """
+            Members:
+
+              ONE
+
+              TWO
+            """
+            def __init__(self, arg0: int) -> None: ...
+            def __int__(self) -> int: ...
+            @property
+            def name(self) -> str:
+                """
+                (self: handle) -> str
+
+                :type: str
+                """
+            ONE: cpp_library_bindings.Outer.Inner.NestedEnum # value = NestedEnum.ONE
+            TWO: cpp_library_bindings.Outer.Inner.NestedEnum # value = NestedEnum.TWO
+            __entries: dict # value = {'ONE': (NestedEnum.ONE, None), 'TWO': (NestedEnum.TWO, None)}
+            __members__: dict # value = {'ONE': NestedEnum.ONE, 'TWO': NestedEnum.TWO}
+            pass
+        @property
+        def value(self) -> Outer.Inner.NestedEnum:
+            """
+            :type: Outer.Inner.NestedEnum
+            """
+        @value.setter
+        def value(self, arg0: Outer.Inner.NestedEnum) -> None:
+            pass
+        pass
+    @property
+    def inner(self) -> Outer.Inner:
+        """
+        :type: Outer.Inner
+        """
+    @inner.setter
+    def inner(self, arg0: Outer.Inner) -> None:
+        pass
     pass
