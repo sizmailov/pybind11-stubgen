0edc9bcde004a31d01d56c2c0de31af432e8e6a4
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-07-28 12:59:34 +0300

    Use git submodule for test pybind11 project

--- test/run_test.sh
+++ test/run_test.sh
@@ -2,16 +2,8 @@
 
 set -e
 
-if [ ! -d pybind11-project-example ]
-then
-    git clone https://github.com/sizmailov/pybind11-project-example.git
-else
-    (cd pybind11-project-example; git pull origin)
-fi
-
 (cd pybind11-project-example; git submodule update --init --recursive)
 (cd pybind11-project-example; python setup.py install)
-
 
 rm -rf "./stubs/generated"
 
