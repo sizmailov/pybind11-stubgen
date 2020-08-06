8a00171075ce3a9f1b5314e0f48566978717ea27
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-08-06 13:53:12 +0300

    fix: __qualname__ is not available before python3.3, fallback to __name__

--- pybind11_stubgen/__init__.py
+++ pybind11_stubgen/__init__.py
@@ -129,7 +129,7 @@ class StubsGenerator(object):
     @staticmethod
     def fully_qualified_name(klass):
         module_name = klass.__module__ if hasattr(klass, '__module__') else None
-        class_name = klass.__qualname__
+        class_name = getattr(klass, "__qualname__", klass.__name__)
 
         if module_name == "builtins":
             return class_name
