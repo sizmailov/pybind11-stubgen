eae2e0fcbc5641691272f18061849e945803a9a5
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-09-04 14:37:48 +0300

    Release 0.8.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     maintainer_email="sergei.a.izmailov@gmail.com",
     description="PEP 561 type stubs generator for pybind11 modules",
     url="https://github.com/sizmailov/pybind11-stubgen",
-    version="0.8.0",
+    version="0.8.1",
     long_description=open("README.rst").read(),
     license="BSD",
     entry_points={'console_scripts': 'pybind11-stubgen = pybind11_stubgen.__init__:main'},
