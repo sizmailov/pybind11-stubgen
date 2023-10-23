476a1f731eb8143f5ccd4a160241aff7479f6205
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2023-10-23 15:16:23 +0900

    chore: Add stubs for pybind11-2.9

---
+++ tests/stubs/python-3.12/pybind11-v2.9/demo/_bindings/enum.pyi
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+import typing
+
+__all__ = [
+    "Blue",
+    "ConsoleForegroundColor",
+    "Green",
+    "Magenta",
+    "None_",
+    "Yellow",
+    "accept_defaulted_enum",
+]
+
+class ConsoleForegroundColor:
+    """
+    Members:
+
+      Green
+
+      Yellow
+
+      Blue
+
+      Magenta
+
+      None_
+    """
+
+    Blue: typing.ClassVar[
+        ConsoleForegroundColor
+    ]  # value = <ConsoleForegroundColor.Blue: 34>
+    Green: typing.ClassVar[
+        ConsoleForegroundColor
+    ]  # value = <ConsoleForegroundColor.Green: 32>
+    Magenta: typing.ClassVar[
+        ConsoleForegroundColor
+    ]  # value = <ConsoleForegroundColor.Magenta: 35>
+    None_: typing.ClassVar[
+        ConsoleForegroundColor
+    ]  # value = <ConsoleForegroundColor.None_: -1>
+    Yellow: typing.ClassVar[
+        ConsoleForegroundColor
+    ]  # value = <ConsoleForegroundColor.Yellow: 33>
+    __members__: typing.ClassVar[
+        dict[str, ConsoleForegroundColor]
+    ]  # value = {'Green': <ConsoleForegroundColor.Green: 32>, 'Yellow': <ConsoleForegroundColor.Yellow: 33>, 'Blue': <ConsoleForegroundColor.Blue: 34>, 'Magenta': <ConsoleForegroundColor.Magenta: 35>, 'None_': <ConsoleForegroundColor.None_: -1>}
+    def __eq__(self, other: typing.Any) -> bool: ...
+    def __getstate__(self) -> int: ...
+    def __hash__(self) -> int: ...
+    def __index__(self) -> int: ...
+    def __init__(self, value: int) -> None: ...
+    def __int__(self) -> int: ...
+    def __ne__(self, other: typing.Any) -> bool: ...
+    def __repr__(self) -> str: ...
+    def __setstate__(self, state: int) -> None: ...
+    def __str__(self) -> str: ...
+    @property
+    def name(self) -> str: ...
+    @property
+    def value(self) -> int: ...
+
+def accept_defaulted_enum(
+    color: ConsoleForegroundColor = ConsoleForegroundColor.None_,
+) -> None: ...
+
+Blue: ConsoleForegroundColor  # value = <ConsoleForegroundColor.Blue: 34>
+Green: ConsoleForegroundColor  # value = <ConsoleForegroundColor.Green: 32>
+Magenta: ConsoleForegroundColor  # value = <ConsoleForegroundColor.Magenta: 35>
+None_: ConsoleForegroundColor  # value = <ConsoleForegroundColor.None_: -1>
+Yellow: ConsoleForegroundColor  # value = <ConsoleForegroundColor.Yellow: 33>
---
+++ tests/stubs/python-3.12/pybind11-v2.9/demo/_bindings/numpy.pyi
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import typing
+
+import numpy
+
+__all__ = [
+    "accept_ndarray_float64",
+    "accept_ndarray_int",
+    "get_ndarray_float64",
+    "get_ndarray_int",
+]
+
+def accept_ndarray_float64(
+    arg0: typing.Annotated[numpy.ndarray, numpy.float64]
+) -> None: ...
+def accept_ndarray_int(arg0: typing.Annotated[numpy.ndarray, numpy.int32]) -> None: ...
+def get_ndarray_float64() -> typing.Annotated[numpy.ndarray, numpy.float64]: ...
+def get_ndarray_int() -> typing.Annotated[numpy.ndarray, numpy.int32]: ...
