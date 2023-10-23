19660059e154f98ad77dc1ee0a80b02db3cac6d7
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2023-10-23 15:08:10 +0900

    fix: install-demo-module.sh

--- tests/install-demo-module.sh
+++ tests/install-demo-module.sh
@@ -15,17 +15,20 @@ function parse_args() {
     fi
     echo "Usage: $0 --pybind11-branch PYBIND11_BRANCH"
     echo "  --pybind11-branch     name of pybind11 branch"
+    echo "  --eigen-branch        name of eigen branch"
     exit 1
   }
 
   # parse params
   while [[ "$#" > 0 ]]; do case $1 in
     --pybind11-branch) PYBIND11_BRANCH="$2"; shift;shift;;
+    --eigen-branch) EIGEN_BRANCH="$2"; shift;shift;;
     *) usage "Unknown parameter passed: $1"; shift; shift;;
   esac; done
 
   # verify params
   if [ -z "$PYBIND11_BRANCH" ]; then usage "PYBIND11_BRANCH is not set"; fi;
+  if [ -z "$EIGEN_BRANCH" ]; then usage "EIGEN_BRANCH is not set"; fi;
 
   TESTS_ROOT="$(readlink -m "$(dirname "$0")")"
   PROJECT_ROOT="${TESTS_ROOT}/.."
@@ -41,7 +44,7 @@ clone_eigen() {
   if [ ! -d "${EXTERNAL_DIR}/eigen" ]; then
     git clone \
         --depth 1 \
-        --branch master \
+        --branch "${EIGEN_BRANCH}" \
         --single-branch \
         https://gitlab.com/libeigen/eigen.git \
         "${EXTERNAL_DIR}/eigen"
@@ -85,8 +88,8 @@ install_demo() {
 install_pydemo() {
   (
     export CMAKE_PREFIX_PATH="$(readlink -m "${INSTALL_PREFIX}")";
-    export CMAKE_ARGS="CMAKE_CXX_STANDARD=17";
-    pip install "${TESTS_ROOT}/py-demo"
+    export CMAKE_ARGS="-DCMAKE_CXX_STANDARD=17";
+    pip install --force-reinstall "${TESTS_ROOT}/py-demo"
   )
 }
 
