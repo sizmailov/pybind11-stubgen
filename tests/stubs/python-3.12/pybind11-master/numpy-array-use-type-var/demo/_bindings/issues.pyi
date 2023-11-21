22f9fa95c122258fc03139b432bebe81e44e6621
Matthew Hoffman <ringohoffman@utexas.edu>
2023-11-20 23:04:15 -0800

    test: ✅ add test output for --numpy-array-use-type-var on 3.12 on pybind11 master

---
+++ tests/stubs/python-3.12/pybind11-master/numpy-array-use-type-var/demo/_bindings/issues.pyi
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+import typing
+
+__all__ = ["issue_51_catastrophic_regex", "issue_73_utf8_doc_chars"]
+
+def issue_51_catastrophic_regex(arg0: int, arg1: int) -> None:
+    """
+    Use-case:
+        issue_51(os.get_handle_inheritable, os.set_handle_inheritable)
+    """
+
+def issue_73_utf8_doc_chars() -> None:
+    """
+    Construct a Ramsete unicycle controller.
+
+    Tuning parameter (b > 0 rad²/m²) for which larger values make
+
+    convergence more aggressive like a proportional term.
+    Tuning parameter (0 rad⁻¹ < zeta < 1 rad⁻¹) for which larger
+    values provide more damping in response.
+    """
+
+_cleanup: typing.Any  # value = <capsule object>
