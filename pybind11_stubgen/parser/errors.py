99f14a1c8486d1ad6a0bdd9ceca81df1531b31c3
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2023-11-27 10:26:56 +0900

    Revert #196 (#203)

--- pybind11_stubgen/parser/errors.py
+++ pybind11_stubgen/parser/errors.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from pybind11_stubgen.structs import Identifier, Import, QualifiedName, Value
+from pybind11_stubgen.structs import Identifier, QualifiedName
 
 
 class ParserError(Exception):
@@ -33,23 +33,3 @@ class NameResolutionError(ParserError):
 
     def __str__(self):
         return f"Can't find/import '{self.name}'"
-
-
-class AmbiguousEnumError(InvalidExpressionError):
-    def __init__(self, repr_: str, *values_and_imports: tuple[Value, Import]):
-        super().__init__(repr_)
-        self.values_and_imports = values_and_imports
-
-        if len(self.values_and_imports) < 2:
-            raise ValueError(
-                "Expected at least 2 values_and_imports, got "
-                f"{len(self.values_and_imports)}"
-            )
-
-    def __str__(self) -> str:
-        origins = sorted(import_.origin for _, import_ in self.values_and_imports)
-        return (
-            f"Enum member '{self.expression}' could not be resolved; multiple "
-            "matching definitions found in: "
-            + ", ".join(f"'{origin}'" for origin in origins)
-        )
--- tests/demo-lib/include/demo/sublibA/ConsoleColors.h
+++ tests/demo-lib/include/demo/sublibA/ConsoleColors.h
@@ -9,14 +9,6 @@ enum class ConsoleForegroundColor {
     None_ = -1
 };
 
-enum class ConsoleForegroundColorDuplicate {
-    Green = 32,
-    Yellow = 33,
-    Blue = 34,
-    Magenta = 35,
-    None_ = -1
-};
-
 enum ConsoleBackgroundColor {
     Green = 42,
     Yellow = 43,
